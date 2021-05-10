a4aca96becfd0af23adb99fd723525973b87d562
thesamurai3011 <thesamurai3011@gmail.com>
2021-05-10 18:12:03 +0700

    sửa lỗi

--- Implement/app/src/main/java/com/example/automativedoor/ui/login/forgot_pass.java
+++ Implement/app/src/main/java/com/example/automativedoor/ui/login/forgot_pass.java
@@ -41,7 +41,7 @@ public class forgot_pass extends AppCompatActivity {
         final EditText usernameEditText = binding.username;
         final EditText passwordEditText = binding.password;
         final Button loginButton = binding.login;
-        final ProgressBar loadingProgressBar = binding.loading;
+
 
         loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
             @Override
@@ -65,7 +65,7 @@ public class forgot_pass extends AppCompatActivity {
                 if (loginResult == null) {
                     return;
                 }
-                loadingProgressBar.setVisibility(View.GONE);
+
                 if (loginResult.getError() != null) {
                     showLoginFailed(loginResult.getError());
                 }
@@ -113,7 +113,7 @@ public class forgot_pass extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadingProgressBar.setVisibility(View.VISIBLE);
+
                 loginViewModel.login(usernameEditText.getText().toString(),
                         passwordEditText.getText().toString());
             }
