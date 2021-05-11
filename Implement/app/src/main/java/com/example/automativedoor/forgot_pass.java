417f54c7bd3f49b3950271e5b62366445f1b8c89
unknown <huan.la54066034@hcmut.edu.vn>
2021-05-11 20:27:22 +0700

    login and logout

--- Implement/app/src/main/java/com/example/automativedoor/forgot_pass.java
+++ Implement/app/src/main/java/com/example/automativedoor/forgot_pass.java
@@ -1,9 +1,12 @@
 package com.example.automativedoor;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.MotionEvent;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
 
@@ -14,6 +17,15 @@ public class forgot_pass extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_pass);
 
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (getCurrentFocus() != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+        return super.dispatchTouchEvent(ev);
     }
 
 }
