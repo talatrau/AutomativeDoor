a216d93d209547b775ddc8f740ea71f281ffd1cf
unknown <huan.la54066034@hcmut.edu.vn>
2021-05-14 17:39:12 +0700

    IOT config

---
+++ Implement/app/src/main/java/com/example/automativedoor/Control/MQTTServer.java
@@ -0,0 +1,105 @@
+package com.example.automativedoor.Control;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class MQTTServer {
+    final String serverUri ="tcp://io.adafruit.com:1883";
+    final String clientId ="[YourclientID]";
+    final String subscriptionTopic ="[Yoursubscriptiontopic]";
+    final String username ="[Yourusername]";
+    final String password ="[Yourpassword]";
+
+    public MqttAndroidClient mqttAndroidClient;
+
+    public MQTTServer(Context context) {
+        this.mqttAndroidClient = new MqttAndroidClient(context, this.serverUri, this.clientId);
+        this.mqttAndroidClient.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+                Log.w("mqtt", serverURI);
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.w("Mqtt", message.toString());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+        connect();
+    }
+
+    public void setCallback(MqttCallbackExtended callback) {
+        this.mqttAndroidClient.setCallback(callback);
+    }
+
+    private void connect() {
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setAutomaticReconnect(true);
+        mqttConnectOptions.setCleanSession(false);
+        mqttConnectOptions.setUserName(this.username);
+        mqttConnectOptions.setPassword(this.password.toCharArray());
+
+        try {
+            this.mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
+                    disconnectedBufferOptions.setBufferEnabled(true);
+                    disconnectedBufferOptions.setBufferSize(100);
+                    disconnectedBufferOptions.setPersistBuffer(false);
+                    disconnectedBufferOptions.setDeleteOldestMessages(false);
+                    mqttAndroidClient.setBufferOpts(disconnectedBufferOptions);
+                    subscribeToTopic();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.w("Mqtt", "Failedtoconnectto:" + serverUri + exception.toString());
+                }
+            });
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void subscribeToTopic() {
+        try {
+            mqttAndroidClient.subscribe(subscriptionTopic, 0, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.w("Mqtt", "Subscribed!");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.w("Mqtt", "Subscribedfail");
+                }
+            });
+
+        } catch (MqttException e) {
+            System.err.println("Exceptionstsubscribing");
+            e.printStackTrace();
+        }
+    }
+}
